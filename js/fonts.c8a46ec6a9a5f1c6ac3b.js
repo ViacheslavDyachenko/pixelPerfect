32bd6d362257d12e795237c5cd9d79c8d11337f6
Viacheslav Dyachenko <dyachenko.v@future-group.ru>
2022-03-21 11:51:17 +0300

    Updates

---
+++ js/fonts.c8a46ec6a9a5f1c6ac3b.js
@@ -0,0 +1,26 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktemplate"] = self["webpackChunktemplate"] || []).push([["fonts"],{
+
+/***/ "/o0F":
+/*!*****************************!*\
+  !*** ./src/styl/fonts.styl ***!
+  \*****************************/
+/***/ (() => {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack://template/./src/styl/fonts.styl?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("/o0F"));
+/******/ }
+]);
---
+++ js/images.8ac650520c21267d8654.js
@@ -0,0 +1,406 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktemplate"] = self["webpackChunktemplate"] || []).push([["images"],{
+
+/***/ "qe1a":
+/*!***************************!*\
+  !*** ./webpack/images.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("__webpack_require__(\"c9hW\");\n\n//# sourceURL=webpack://template/./webpack/images.js?");
+
+/***/ }),
+
+/***/ "ApAj":
+/*!*****************************!*\
+  !*** ./src/images/alfa.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/alfa.svg\";\n\n//# sourceURL=webpack://template/./src/images/alfa.svg?");
+
+/***/ }),
+
+/***/ "ZRwS":
+/*!***********************************!*\
+  !*** ./src/images/arrow-left.svg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/arrow-left.svg\";\n\n//# sourceURL=webpack://template/./src/images/arrow-left.svg?");
+
+/***/ }),
+
+/***/ "I5Lz":
+/*!************************************!*\
+  !*** ./src/images/arrow-right.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/arrow-right.svg\";\n\n//# sourceURL=webpack://template/./src/images/arrow-right.svg?");
+
+/***/ }),
+
+/***/ "rSxF":
+/*!*******************************!*\
+  !*** ./src/images/avatar.png ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/avatar.png\";\n\n//# sourceURL=webpack://template/./src/images/avatar.png?");
+
+/***/ }),
+
+/***/ "rM7U":
+/*!**********************************!*\
+  !*** ./src/images/bank-open.svg ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/bank-open.svg\";\n\n//# sourceURL=webpack://template/./src/images/bank-open.svg?");
+
+/***/ }),
+
+/***/ "4UXT":
+/*!********************************!*\
+  !*** ./src/images/browser.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/browser.svg\";\n\n//# sourceURL=webpack://template/./src/images/browser.svg?");
+
+/***/ }),
+
+/***/ "3CT9":
+/*!******************************!*\
+  !*** ./src/images/check.svg ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/check.svg\";\n\n//# sourceURL=webpack://template/./src/images/check.svg?");
+
+/***/ }),
+
+/***/ "u2IB":
+/*!******************************!*\
+  !*** ./src/images/cloud.svg ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/cloud.svg\";\n\n//# sourceURL=webpack://template/./src/images/cloud.svg?");
+
+/***/ }),
+
+/***/ "VSQ6":
+/*!*****************************!*\
+  !*** ./src/images/coin.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/coin.svg\";\n\n//# sourceURL=webpack://template/./src/images/coin.svg?");
+
+/***/ }),
+
+/***/ "s7W7":
+/*!*************************************!*\
+  !*** ./src/images/company-logo.svg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/company-logo.svg\";\n\n//# sourceURL=webpack://template/./src/images/company-logo.svg?");
+
+/***/ }),
+
+/***/ "/B4B":
+/*!*******************************!*\
+  !*** ./src/images/corner.svg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/corner.svg\";\n\n//# sourceURL=webpack://template/./src/images/corner.svg?");
+
+/***/ }),
+
+/***/ "60x3":
+/*!*******************************!*\
+  !*** ./src/images/dollar.svg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/dollar.svg\";\n\n//# sourceURL=webpack://template/./src/images/dollar.svg?");
+
+/***/ }),
+
+/***/ "AJiz":
+/*!*********************************!*\
+  !*** ./src/images/exit-btn.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/exit-btn.svg\";\n\n//# sourceURL=webpack://template/./src/images/exit-btn.svg?");
+
+/***/ }),
+
+/***/ "TG2x":
+/*!*********************************!*\
+  !*** ./src/images/facebook.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/facebook.svg\";\n\n//# sourceURL=webpack://template/./src/images/facebook.svg?");
+
+/***/ }),
+
+/***/ "nY+7":
+/*!*********************************!*\
+  !*** ./src/images/feedback.png ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/feedback.png\";\n\n//# sourceURL=webpack://template/./src/images/feedback.png?");
+
+/***/ }),
+
+/***/ "hqFr":
+/*!************************************!*\
+  !*** ./src/images/future-mark.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/future-mark.svg\";\n\n//# sourceURL=webpack://template/./src/images/future-mark.svg?");
+
+/***/ }),
+
+/***/ "x99D":
+/*!***********************************!*\
+  !*** ./src/images/icon-check.svg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/icon-check.svg\";\n\n//# sourceURL=webpack://template/./src/images/icon-check.svg?");
+
+/***/ }),
+
+/***/ "To7U":
+/*!*****************************!*\
+  !*** ./src/images/lock.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/lock.svg\";\n\n//# sourceURL=webpack://template/./src/images/lock.svg?");
+
+/***/ }),
+
+/***/ "Uf5n":
+/*!*********************************!*\
+  !*** ./src/images/main-bgc.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/main-bgc.svg\";\n\n//# sourceURL=webpack://template/./src/images/main-bgc.svg?");
+
+/***/ }),
+
+/***/ "keHQ":
+/*!******************************!*\
+  !*** ./src/images/minus.svg ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/minus.svg\";\n\n//# sourceURL=webpack://template/./src/images/minus.svg?");
+
+/***/ }),
+
+/***/ "u8Vu":
+/*!****************************!*\
+  !*** ./src/images/mkb.svg ***!
+  \****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/mkb.svg\";\n\n//# sourceURL=webpack://template/./src/images/mkb.svg?");
+
+/***/ }),
+
+/***/ "v+7S":
+/*!**********************************!*\
+  !*** ./src/images/pleer-btn.svg ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/pleer-btn.svg\";\n\n//# sourceURL=webpack://template/./src/images/pleer-btn.svg?");
+
+/***/ }),
+
+/***/ "lqka":
+/*!*************************************!*\
+  !*** ./src/images/presentation.svg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/presentation.svg\";\n\n//# sourceURL=webpack://template/./src/images/presentation.svg?");
+
+/***/ }),
+
+/***/ "OJLj":
+/*!***************************************!*\
+  !*** ./src/images/question-block.png ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/question-block.png\";\n\n//# sourceURL=webpack://template/./src/images/question-block.png?");
+
+/***/ }),
+
+/***/ "+oCn":
+/*!**********************************!*\
+  !*** ./src/images/red-minus.svg ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/red-minus.svg\";\n\n//# sourceURL=webpack://template/./src/images/red-minus.svg?");
+
+/***/ }),
+
+/***/ "Qs16":
+/*!*********************************!*\
+  !*** ./src/images/red-plus.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/red-plus.svg\";\n\n//# sourceURL=webpack://template/./src/images/red-plus.svg?");
+
+/***/ }),
+
+/***/ "owhH":
+/*!************************************!*\
+  !*** ./src/images/review-logo.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/review-logo.svg\";\n\n//# sourceURL=webpack://template/./src/images/review-logo.svg?");
+
+/***/ }),
+
+/***/ "bMo+":
+/*!********************************!*\
+  !*** ./src/images/rosbank.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/rosbank.svg\";\n\n//# sourceURL=webpack://template/./src/images/rosbank.svg?");
+
+/***/ }),
+
+/***/ "vndu":
+/*!*****************************!*\
+  !*** ./src/images/sber.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/sber.svg\";\n\n//# sourceURL=webpack://template/./src/images/sber.svg?");
+
+/***/ }),
+
+/***/ "4byE":
+/*!********************************!*\
+  !*** ./src/images/twitter.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/twitter.svg\";\n\n//# sourceURL=webpack://template/./src/images/twitter.svg?");
+
+/***/ }),
+
+/***/ "vqAp":
+/*!*********************************!*\
+  !*** ./src/images/umbrella.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/umbrella.svg\";\n\n//# sourceURL=webpack://template/./src/images/umbrella.svg?");
+
+/***/ }),
+
+/***/ "j1+h":
+/*!******************************!*\
+  !*** ./src/images/video.png ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/video.png\";\n\n//# sourceURL=webpack://template/./src/images/video.png?");
+
+/***/ }),
+
+/***/ "d9Uc":
+/*!****************************!*\
+  !*** ./src/images/vtb.svg ***!
+  \****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/vtb.svg\";\n\n//# sourceURL=webpack://template/./src/images/vtb.svg?");
+
+/***/ }),
+
+/***/ "7LDF":
+/*!********************************!*\
+  !*** ./src/images/webinar.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/webinar.svg\";\n\n//# sourceURL=webpack://template/./src/images/webinar.svg?");
+
+/***/ }),
+
+/***/ "6eIA":
+/*!**************************************!*\
+  !*** ./src/images/why-nrd-photo.png ***!
+  \**************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/why-nrd-photo.png\";\n\n//# sourceURL=webpack://template/./src/images/why-nrd-photo.png?");
+
+/***/ }),
+
+/***/ "4T1C":
+/*!********************************!*\
+  !*** ./src/images/youtube.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/youtube.svg\";\n\n//# sourceURL=webpack://template/./src/images/youtube.svg?");
+
+/***/ }),
+
+/***/ "bkvD":
+/*!************************************!*\
+  !*** ./src/images/сorporation.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/сorporation.svg\";\n\n//# sourceURL=webpack://template/./src/images/%D1%81orporation.svg?");
+
+/***/ }),
+
+/***/ "c9hW":
+/*!***********************************************************!*\
+  !*** ./src/images/ sync \.(svg%7Cpng%7Cjpg%7Cico%7Cgif)$ ***!
+  \***********************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var map = {\n\t\"./alfa.svg\": \"ApAj\",\n\t\"./arrow-left.svg\": \"ZRwS\",\n\t\"./arrow-right.svg\": \"I5Lz\",\n\t\"./avatar.png\": \"rSxF\",\n\t\"./bank-open.svg\": \"rM7U\",\n\t\"./browser.svg\": \"4UXT\",\n\t\"./check.svg\": \"3CT9\",\n\t\"./cloud.svg\": \"u2IB\",\n\t\"./coin.svg\": \"VSQ6\",\n\t\"./company-logo.svg\": \"s7W7\",\n\t\"./corner.svg\": \"/B4B\",\n\t\"./dollar.svg\": \"60x3\",\n\t\"./exit-btn.svg\": \"AJiz\",\n\t\"./facebook.svg\": \"TG2x\",\n\t\"./feedback.png\": \"nY+7\",\n\t\"./future-mark.svg\": \"hqFr\",\n\t\"./icon-check.svg\": \"x99D\",\n\t\"./lock.svg\": \"To7U\",\n\t\"./main-bgc.svg\": \"Uf5n\",\n\t\"./minus.svg\": \"keHQ\",\n\t\"./mkb.svg\": \"u8Vu\",\n\t\"./pleer-btn.svg\": \"v+7S\",\n\t\"./presentation.svg\": \"lqka\",\n\t\"./question-block.png\": \"OJLj\",\n\t\"./red-minus.svg\": \"+oCn\",\n\t\"./red-plus.svg\": \"Qs16\",\n\t\"./review-logo.svg\": \"owhH\",\n\t\"./rosbank.svg\": \"bMo+\",\n\t\"./sber.svg\": \"vndu\",\n\t\"./twitter.svg\": \"4byE\",\n\t\"./umbrella.svg\": \"vqAp\",\n\t\"./video.png\": \"j1+h\",\n\t\"./vtb.svg\": \"d9Uc\",\n\t\"./webinar.svg\": \"7LDF\",\n\t\"./why-nrd-photo.png\": \"6eIA\",\n\t\"./youtube.svg\": \"4T1C\",\n\t\"./сorporation.svg\": \"bkvD\"\n};\n\n\nfunction webpackContext(req) {\n\tvar id = webpackContextResolve(req);\n\treturn __webpack_require__(id);\n}\nfunction webpackContextResolve(req) {\n\tif(!__webpack_require__.o(map, req)) {\n\t\tvar e = new Error(\"Cannot find module '\" + req + \"'\");\n\t\te.code = 'MODULE_NOT_FOUND';\n\t\tthrow e;\n\t}\n\treturn map[req];\n}\nwebpackContext.keys = function webpackContextKeys() {\n\treturn Object.keys(map);\n};\nwebpackContext.resolve = webpackContextResolve;\nmodule.exports = webpackContext;\nwebpackContext.id = \"c9hW\";\n\n//# sourceURL=webpack://template/./src/images/_sync_\\.(svg%257Cpng%257Cjpg%257Cico%257Cgif)$?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("qe1a"));
+/******/ }
+]);
